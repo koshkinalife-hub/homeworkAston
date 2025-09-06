c5bcd33dfc3da68c91cdb82a5a11409905f2e6d8
User <git@github.com:koshkinalife-hub/homeworkAston.git>
2025-09-06 12:47:58 +0300

    метод проверка 8,9 пунктов +

--- src/main/java/org/aston/Main.java
+++ src/main/java/org/aston/Main.java
@@ -15,6 +15,16 @@ public class Main {
         printnumberSign(0);
         printnumberSign(81);
         printnumberSign(-23);
+        // true false для 7 задания
+        System.out.println(isNegative(-13));
+        System.out.println(isNegative(0));
+        System.out.println(isNegative(94));
+        // ввод строки несколько раз
+        printString("Хоккейный сезон начался", 5);
+        // вопрос о високосный ли год
+        System.out.println(isYear (2000)); //на 400 делится
+        System.out.println(isYear(1700)); //на 100
+        System.out.println(isYear(2025));
     }
 
     public static void printThreeWords() {
@@ -73,5 +83,34 @@ public static boolean isSumWithinRange(int a, int b) {
         else {
             System.out.println("Число отрицательное");
         }
+    }
+
+    //пункт 7 домашки
+    public static boolean isNegative(int number) {
+        return number < 0;
+    }
+
+    // пункт 8 домашки строка в несколько раз
+    public static void printString (String str, int times) {
+        for (int i = 0; i< times; i++) {
+            System.out.println(str);
+        }
+    }
+    // пункт 9 домашки
+    public static boolean isYear (int year) {
+        // делится на 400 = високосный/сначала проверка на 400,100,4 чтобы не сбить логичку!
+        if (year % 400 == 0) {
+            return true;
+        }
+        // делится на 100 = не високосный
+        if (year % 100 == 0) {
+            return false;
+        }
+        // делится на 4 = високосный
+        if (year % 4 == 0 ){
+            return true;
+        }
+        // все остальные года не високосные
+       else return false;
     }
     }
